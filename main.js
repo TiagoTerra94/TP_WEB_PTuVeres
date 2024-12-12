23a907483a330156e5bf3204e3af303156c17e72
TiagoTerra94 <coio_terra@hotmail.com>
2024-12-12 09:12:18 +0000

    TrabalhoPratico

---
+++ main.js
@@ -0,0 +1,11 @@
+//Função Toast
+const toastTrigger = document.getElementById('toastTrigger')
+const toastAlert = document.getElementById('toastAlert')
+
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    
+    const toast = new bootstrap.Toast(toastAlert);
+    toast.show();
+  });
+}
